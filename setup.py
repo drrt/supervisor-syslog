51d621886e587ec5be40a75a133ce4140a1562c1
David Langhorst <davidl@eatsa.com>
2017-03-17 18:09:12 -0700

    requirements, typo

--- setup.py
+++ setup.py
@@ -2,11 +2,12 @@
 
 from setuptools import setup
 
-setup(name='sypervisor-syslog',
+setup(name='supervisor-syslog',
     version='0.1.0',
     description='supervisor event handler for syslogging',
     packages=['supervisor_syslog'],
     setup_requires=['pytest-runner'],
+    install_requires=['PyYAML>=3.0'],
     tests_require=['pytest'],
     entry_points={'console_scripts': ['supervisor-syslog=supervisor_syslog:handler']}
     )
